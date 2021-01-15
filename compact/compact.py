4733de00d90b52cad6730943bf333c60e8a10e56
nesseri <eric.ness@chrobinson.com>
2021-01-14 19:59:55 -0600

    Finish compact bonuses

--- compact/compact.py
+++ compact/compact.py
@@ -2,9 +2,7 @@ from typing import Sequence, Iterable
 
 
 def compact(values: Sequence) -> Iterable:
-    result = []
     for value in values:
         if not 'last' in locals() or not value == last or (value is None and last is None):
-            result.append(value)
+            yield value
         last = value
-    return result
