50715808bc9d257465040dc2f3c7281540d28b1b
nesseri <eric.ness@chrobinson.com>
2021-02-04 09:00:44 -0600

    Base timer solution

---
+++ timer/timer.py
@@ -0,0 +1,25 @@
+import time
+
+
+class Timer:
+    """Time an operation"""
+
+    def __init__(self):
+        self.start_time = 0.0
+        self.end_time = 0.0
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Exception = None,
+        exc_value: BaseException = None,
+        traceback=None,
+    ):
+        self.end_time = time.time()
+
+    @property
+    def elapsed(self) -> float:
+        return self.end_time - self.start_time
