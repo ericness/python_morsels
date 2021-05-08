c7023c70a9d2ec71281b402fe08d81dc279dda76
nesseri <eric.ness@chrobinson.com>
2021-05-08 18:36:32 -0500

    perfect_square bonus 1

--- perfect_square/perfect_square.py
+++ perfect_square/perfect_square.py
@@ -7,5 +7,8 @@ def is_perfect_square(value: SupportsFloat) -> bool:
     """Determine if number is perfect square"""
     if not isinstance(value, SupportsFloat):
         raise TypeError("Argument is not a number")
+    if value < 0:
+        return False
+
     root = math.sqrt(value)
     return math.ceil(root) ** 2 == value or math.floor(root) ** 2 == value
