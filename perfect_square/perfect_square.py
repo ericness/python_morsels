0c490088bdc1ead3552b949c7f5735e4d77f6cb8
nesseri <eric.ness@chrobinson.com>
2021-05-08 18:34:21 -0500

    perfect_square base solution

---
+++ perfect_square/perfect_square.py
@@ -0,0 +1,11 @@
+import math
+from numbers import Number
+from typing import SupportsFloat
+
+
+def is_perfect_square(value: SupportsFloat) -> bool:
+    """Determine if number is perfect square"""
+    if not isinstance(value, SupportsFloat):
+        raise TypeError("Argument is not a number")
+    root = math.sqrt(value)
+    return math.ceil(root) ** 2 == value or math.floor(root) ** 2 == value
