2d780a2c05419b1b2bfeb10bf373d845d5ee4c41
nesseri <eric.ness@chrobinson.com>
2021-05-13 07:46:30 -0500

    perfect_square bonus 3

--- perfect_square/perfect_square.py
+++ perfect_square/perfect_square.py
@@ -1,18 +1,23 @@
+import cmath
 import math
 import decimal
 from numbers import Number
 from typing import SupportsFloat
 
 
-def is_perfect_square(value: SupportsFloat) -> bool:
+def is_perfect_square(value: SupportsFloat, *, complex: bool = False) -> bool:
     """Determine if number is perfect square"""
-    if not isinstance(value, SupportsFloat):
-        raise TypeError("Argument is not a number")
-    if value < 0:
-        return False
+    if not complex:
+        if not isinstance(value, SupportsFloat):
+            raise TypeError("Argument is not a number")
+        if value < 0:
+            return False
 
-    with decimal.localcontext() as c:
-        c.prec = 50
-        decimal_value = decimal.Decimal(value)
-        root = decimal_value.sqrt()
-        return root.to_integral_value() ** 2 == decimal_value
+        with decimal.localcontext() as c:
+            c.prec = 50
+            decimal_value = decimal.Decimal(value)
+            root = decimal_value.sqrt()
+            return root.to_integral_value() ** 2 == decimal_value
+    else:
+        root = cmath.sqrt(value)
+        return root.real.is_integer() and root.imag.is_integer()
--- perfect_square/test_perfect_square.py
+++ perfect_square/test_perfect_square.py
@@ -55,7 +55,6 @@ class IsPerfectSquareTests(unittest.TestCase):
         self.assertFalse(is_perfect_square(m+1))
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
     def test_complex_numbers(self):
         self.assertTrue(is_perfect_square(-4, complex=True))
         self.assertTrue(is_perfect_square(-100, complex=True))
