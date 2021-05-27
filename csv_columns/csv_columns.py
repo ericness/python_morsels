87693b843fde5300000f28d66925704739cd2ff1
nesseri <eric.ness@chrobinson.com>
2021-05-27 09:17:36 -0500

    csv_columns bonus 2

--- csv_columns/csv_columns.py
+++ csv_columns/csv_columns.py
@@ -1,15 +1,19 @@
 from collections import defaultdict, OrderedDict
 import csv
-from typing import TextIO, Dict
+from typing import TextIO, Dict, List
 
 
-def csv_columns(file: TextIO) -> Dict:
+def csv_columns(file: TextIO, headers: List[str] = None) -> Dict:
     """Map column headers to lists of values"""
     values = defaultdict(list)
     csv_reader = csv.reader(file)
     for i, row in enumerate(csv_reader):
-        if i == 0:
+        if i == 0 and not headers:
             keys = row
+        elif i == 0 and headers:
+            keys = headers
+            for j, value in enumerate(row):
+                values[j].append(value)
         else:
             for j, value in enumerate(row):
                 values[j].append(value)
