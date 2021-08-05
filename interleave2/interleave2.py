041c7fb8d1291b73d6d598d19384668f6dcb83b9
ericness <eric.christopher.ness@gmail.com>
2021-08-04 21:12:19 -0500

    Interleave2 bonus 2 and 3

--- interleave2/interleave2.py
+++ interleave2/interleave2.py
@@ -3,14 +3,9 @@ from typing import Iterable, Iterator
 
 DUMMY = "__no_value__"
 
-def interleave(iterable1: Iterable, iterable2: Iterable) -> Iterator:
+def interleave(*iterable_args) -> Iterator:
     """Interleave the elements of multiple iterables"""
-    for element1, element2 in itertools.zip_longest(iterable1, iterable2, fillvalue=DUMMY):
-       if element1 != DUMMY:
-            yield element1
-       if element2 != DUMMY:
-            yield element2
-
-if __name__ == "__main__":
-    result = interleave([], [])
-    print(result)
+    for elements in itertools.zip_longest(*iterable_args, fillvalue=DUMMY):
+         for element in elements:
+             if element != DUMMY:
+                 yield element
